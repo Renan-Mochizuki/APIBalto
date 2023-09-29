3b972d5e4d44f906ad9e8ae1fcb080ef750263a9
Renan Mochizuki <renanmochizuki@gmail.com>
2023-09-28 21:43:02 -0300

    Adicionado tela home na API

--- server/index.js
+++ server/index.js
@@ -4,26 +4,27 @@ const cors = require('cors');
 const favicon = require('serve-favicon');
 const path = require('path');
 const bodyParser = require('body-parser');
-const autenticacao = require("./autenticacaoserver");
-const cadastro = require("./cadastrarserver");
-const alteracao = require("./alterarserver");
-const selecao = require("./selecionarserver");
-const exclusao = require("./deletarserver");
+const autenticacao = require("./autenticacaoRoute");
+const cadastro = require("./cadastrarRoute");
+const alteracao = require("./alterarRoute");
+const selecao = require("./selecionarRoute");
+const selecaoImagem = require("./selecionarImagemRoute");
+const exclusao = require("./deletarRoute");
 
 let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('API Balto');
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
 app.use(favicon(path.join(__dirname, '..', 'public', 'img', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(autenticacao);
 app.use(cadastro);
 app.use(alteracao);
 app.use(selecao);
+app.use(selecaoImagem);
 app.use(exclusao);
 
 const port = 3000;
---
+++ server/selecionarImagemRoute.js
@@ -0,0 +1,108 @@
+const express = require('express');
+let model = require('../models');
+
+let selecaoImagem = express();
+
+selecaoImagem.get('/selpessoaimg/:TB_PESSOA_ID', async (req, res) => {
+    try {
+        const TB_PESSOA_ID = req.params.TB_PESSOA_ID;
+        const campo = await model.TB_PESSOA.findByPk(TB_PESSOA_ID);
+
+        if (!campo)
+            return res.status(404).json({ message: 'Campo não encontrado' });
+        if (campo.TB_PESSOA_IMG == null)
+            return res.status(404).json({ message: 'Imagem não cadastrada' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(campo.TB_PESSOA_IMG);
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro ao buscar imagem' });
+    }
+});
+
+selecaoImagem.get('/selanimalimg/:TB_ANIMAL_ID', async (req, res) => {
+    try {
+        const TB_ANIMAL_ID = req.params.TB_ANIMAL_ID;
+        const campo = await model.TB_ANIMAL.findByPk(TB_ANIMAL_ID);
+
+        if (!campo)
+            return res.status(404).json({ message: 'Campo não encontrado' });
+        if (campo.TB_ANIMAL_IMG1 == null)
+            return res.status(404).json({ message: 'Imagem não cadastrada' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(campo.TB_ANIMAL_IMG1);
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro ao buscar imagem' });
+    }
+});
+
+selecaoImagem.get('/selmensagemimg/:TB_MENSAGEM_ID', async (req, res) => {
+    try {
+        const TB_MENSAGEM_ID = req.params.TB_MENSAGEM_ID;
+        const campo = await model.TB_MENSAGEM.findByPk(TB_MENSAGEM_ID);
+
+        if (!campo)
+            return res.status(404).json({ message: 'Campo não encontrado' });
+        if (campo.TB_MENSAGEM_IMG == null)
+            return res.status(404).json({ message: 'Imagem não cadastrada' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(campo.TB_MENSAGEM_IMG);
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro ao buscar imagem' });
+    }
+});
+
+selecaoImagem.get('/selformulariodiarioimg/:TB_FORMULARIO_DIARIO_ID', async (req, res) => {
+    try {
+        const TB_FORMULARIO_DIARIO_ID = req.params.TB_FORMULARIO_DIARIO_ID;
+        const campo = await model.TB_FORMULARIO_DIARIO.findByPk(TB_FORMULARIO_DIARIO_ID);
+
+        if (!campo)
+            return res.status(404).json({ message: 'Campo não encontrado' });
+        if (campo.TB_FORMULARIO_DIARIO_IMG == null)
+            return res.status(404).json({ message: 'Imagem não cadastrada' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(campo.TB_FORMULARIO_DIARIO_IMG);
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro ao buscar imagem' });
+    }
+});
+
+selecaoImagem.get('/selpostagemimg/:TB_POSTAGEM_ID', async (req, res) => {
+    try {
+        const TB_POSTAGEM_ID = req.params.TB_POSTAGEM_ID;
+        const campo = await model.TB_POSTAGEM.findByPk(TB_POSTAGEM_ID);
+
+        if (!campo)
+            return res.status(404).json({ message: 'Campo não encontrado' });
+        if (campo.TB_POSTAGEM_IMG1 == null)
+            return res.status(404).json({ message: 'Imagem não cadastrada' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(campo.TB_POSTAGEM_IMG1);
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro ao buscar imagem' });
+    }
+});
+
+selecaoImagem.get('/seldenunciaimg/:TB_DENUNCIA_ID', async (req, res) => {
+    try {
+        const TB_DENUNCIA_ID = req.params.TB_DENUNCIA_ID;
+        const campo = await model.TB_DENUNCIA.findByPk(TB_DENUNCIA_ID);
+
+        if (!campo)
+            return res.status(404).json({ message: 'Campo não encontrado' });
+        if (campo.TB_DENUNCIA_IMG1 == null)
+            return res.status(404).json({ message: 'Imagem não cadastrada' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(campo.TB_DENUNCIA_IMG1);
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro ao buscar imagem' });
+    }
+});
+
+module.exports = selecaoImagem;
